27c7feec2a0cd89da695d6cc045a849b3b0cdbb5
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-19 14:27:33 -0400

    completed romanCharValue function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,19 @@
 #include <iostream>
 using namespace std;
 
+
+int romanCharValue(char r);
 int main(void){
     
+    
+}
+
+int romanCharValue(char r){
+    if(r == 'I') return 1;
+    if(r == 'V') return 5;
+    if(r == 'X') return 10;
+    if(r == 'L') return 50;
+    if(r == 'C') return 100;
+    if(r == 'D') return 500;
+    if(r == 'M') return 1000;
 }
