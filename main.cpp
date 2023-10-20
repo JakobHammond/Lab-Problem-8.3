0c37f8c7242acc8898fce59d9cebb3e3b276ef23
JakobHammond <143117911+JakobHammond@users.noreply.github.com>
2023-10-19 23:15:38 -0400

    completed convertRomanToInt function

--- main.cpp
+++ main.cpp
@@ -10,7 +10,11 @@ using namespace std;
 
 
 int romanCharValue(char r);
+int convertRomanToInt(string s);
+int countCharacter(string s);
 int main(void){
+    int value;
+    
     
     
 }
@@ -23,4 +27,34 @@ int romanCharValue(char r){
     if(r == 'C') return 100;
     if(r == 'D') return 500;
     if(r == 'M') return 1000;
+    else return 0;
+}
+
+int convertRomanToInt(string s){
+    int value = 0;
+    for(int i = 0; i < countCharacter(s); i++){
+        string current;
+        string right;
+        int valCurrent;
+        int valRight = 0;
+        
+       
+        current = s.substr(i);
+        
+        right = s.substr(i + 1);
+        char convertRight = right.front();
+        valRight = romanCharValue(convertRight);
+        
+        char convertCurrent = current.front();
+        valCurrent = romanCharValue(convertCurrent);
+        if(valCurrent < valRight){
+            valCurrent = valCurrent * -1;
+        }
+        value += valCurrent;
+    }
+    return value;
+}
+
+int countCharacter(string s){
+    return s.length();
 }
